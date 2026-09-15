6d88f42e3dad60488c6429a947dcbe9e4d17c0d0
Ranveer1332 <chawlaranveer2006@gmail.com>
2026-09-16 00:20:31 +0530

    Add provider dependency and render cart items in CartPage ListView

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
+import 'global_variable.dart';
+
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+      appBar: AppBar(
+        title: Text('Cart'),
       ),
+      body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index) {
+                final cartItem = cart[index];
+
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+                    radius: 30,
+                  ),
+                  trailing: IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.delete,color: Colors.red,)
+                  ),
+                  title: Text(
+                    cartItem['title'].toString(),
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  subtitle: Text('Size : ${cartItem['size']}'),
+                );
+
+      }),
     );
   }
 }
